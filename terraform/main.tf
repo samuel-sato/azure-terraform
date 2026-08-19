5955c23887f08152b75e44d59899a196e24c35c3
Samuel Sato <samuelsato2009@gmail.com>
2026-08-19 17:25:37 -0300

    Ajuste vm_size node kubernets

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   # Default Node Pool - Minimal for labs
   default_node_pool {
     name            = "default"
-    vm_size         = "Standard_B2s"
+    vm_size         = "Standard_D2als_v6"
     node_count      = 1
     os_disk_size_gb = 30
     type            = "VirtualMachineScaleSets"
